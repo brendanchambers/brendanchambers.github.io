9bb8fb45bc94394afd98e74946a8e6f90d236671
br ch <brendanchambers@uchicago.edu>
2018-06-04 22:43:13 -0400

    toggling menu

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+// wait for page to load
+
+document.addEventListener("DOMContentLoaded", function() { // document ready
+
+	// menu declarations
+	let menu_trigger = document.querySelector('.nav-bar');
+	let menu_bar = document.querySelector('.nav-unrolled-menu');
+
+
+	// menu functionality
+	function unrollMenu() {
+		let menuStatus = menu_bar.style.visibility;
+		console.log(String(menuStatus));
+		if (menuStatus === 'visible') {
+
+			menu_bar.style.visibility = 'collapse'; // collapse
+		} else if (menuStatus === 'collapse') {
+			menu_bar.style.visibility = 'visible';  // show
+		} else {
+			menu_bar.style.visibility = 'visible'; // handle the initial case
+		}
+		
+		console.log('unroll menu');
+	}
+
+
+	// menu event listeners
+	menu_trigger.addEventListener('click',unrollMenu)
+	
+
+}); // document ready enclosure
+
