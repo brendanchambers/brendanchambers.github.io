779128b4d88d945ed11285ef8aea62747b69a763
brendanchambers <brendanchambers@gmail.com>
2018-07-05 17:13:32 -0500

    menu unrolling with toggle class

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 // wait for page to load
-
+/*
 document.addEventListener("DOMContentLoaded", function() { // document ready
 
 	// menu declarations
@@ -30,4 +30,27 @@ document.addEventListener("DOMContentLoaded", function() { // document ready
 	
 
 }); // document ready enclosure
+
+*/
+
+
+document.addEventListener("DOMContentLoaded", function() { // document ready
+
+	// menu declarations
+	let menu_trigger = document.querySelector('.nav-bar');
+	let menu_bar = document.querySelector('.nav-main-menu');
+
+
+	// menu functionality
+	function unrollMenu() {
+		menu_bar.classList.toggle('unrolled');
+	}
+
+
+	// menu event listeners
+	menu_trigger.addEventListener('click',unrollMenu)
+	
+
+}); // document ready enclosure
+
 
